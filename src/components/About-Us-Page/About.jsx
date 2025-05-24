2b2747985efe91827c23f06c10a3cd6d1e6edcaf
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-24 22:33:39 +0100

    other modules update for deployment

--- src/components/About-Us-Page/About.jsx
+++ src/components/About-Us-Page/About.jsx
@@ -6,25 +6,23 @@ import Testimonials from "./Testimonials";
 import Clients from "./Clients";
 import Footer from "./Footer";
 
-
 const AboutUs = () => {
   const backgroundImages = [
     {
       src: "/src/assets/Hero-background1.png",
-      text: "Bring your brand to life—beautifully, effortlessly."
+      text: "Bring your brand to life—beautifully, effortlessly.",
     },
-  
+
     {
       src: "/src/assets/Hero-background2.jpg",
-      text: "Design less. Impress more.Let Brandwave do the work"
+      text: "Design less. Impress more.Let Brandwave do the work",
     },
-  
+
     {
       src: "/src/assets/Hero-background3.jpg",
-      text: "Elevate your story. Empower your brand. Engage the world"
-    }
-  ]
-  
+      text: "Elevate your story. Empower your brand. Engage the world",
+    },
+  ];
 
   return (
     <div>
@@ -32,11 +30,11 @@ const AboutUs = () => {
       <Services />
       <Info />
       <Processes />
-      <Testimonials title='From Our Satisfied Clients' />
+      <Testimonials title="From Our Satisfied Clients" />
       <Clients />
       <Footer />
     </div>
-  )
+  );
 };
 
 export default AboutUs;
--- src/components/About-Us-Page/Clients.jsx
+++ src/components/About-Us-Page/Clients.jsx
@@ -1,27 +1,26 @@
 const clientLogo = [
-    '/src/assets/client1.png',
-    '/src/assets/client2.png',
-    '/src/assets/client3.png',
-    '/src/assets/client4.png',
-    '/src/assets/client5.png',
-    '/src/assets/client6.png',
-    '/src/assets/client7.png',
-    '/src/assets/client8.png'
-]
+  "/src/assets/client1.png",
+  "/src/assets/client2.png",
+  "/src/assets/client3.png",
+  "/src/assets/client4.png",
+  "/src/assets/client5.png",
+  "/src/assets/client6.png",
+  "/src/assets/client7.png",
+  "/src/assets/client8.png",
+];
 
 const Clients = () => {
-    return (
-        <section className="client_container">
-            <div className="client_logos">
-                {clientLogo.concat(clientLogo).map((client, index) => (
-                    <div key={index} className="client_image">
-                        <img src={client} alt="client" />
-                    </div>
-                ))}
-            </div>
-            
-        </section>
-    )
+  return (
+    <section className="client_container">
+      <div className="client_logos">
+        {clientLogo.concat(clientLogo).map((client, index) => (
+          <div key={index} className="client_image">
+            <img src={client} alt="client" />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
